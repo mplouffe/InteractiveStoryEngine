d4b1897411c82dd175ffc9ad22f6fe4c13d5c413
Matheu Plouffe <matheuplouffe@gmail.com>
2023-01-10 22:57:04 -0600

    Initial commit.

---
+++ Assets/Scripts/DialogueWindow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueWindow : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_dialogueText;
+
+    [SerializeField]
+    private RectTransform m_dialogueTransform;
+
+    [SerializeField]
+    private TextMeshProUGUI m_nameText;
+
+    [SerializeField]
+    private RectTransform m_nameRectTransform;
+
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+### Interactive Story Engine
+
+<p align="center">
+  <img src="/Documents/Screencap_01.png">
+</p>
+
+A little dialogue system for my Unity games.
+
+Made with :blue_heart:
